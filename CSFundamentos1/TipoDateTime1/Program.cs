700fce444a5eed60bdfc39e90e054be215b9c7bb
Alfonso Noerenberg Junior <alfonso.junior@evolutize.com.br>
2026-03-05 19:45:20 -0300

    Tipos de dados referenciados e DateTime

---
+++ CSFundamentos1/TipoDateTime1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("## Struct DateTime ##\n");
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine($"Data atual: {dataAtual}");
+
+DateTime dataEspecifica = new DateTime(2026, 3, 5);
+Console.WriteLine($"Data específica: {dataEspecifica}");
+
+DateTime dataComHora = new DateTime(2026, 3, 5, 14, 30, 0);
+Console.WriteLine($"Data com hora: {dataComHora}");
+
+Console.WriteLine($"\nAno: {dataAtual.Year}");
+Console.WriteLine($"Mês: {dataAtual.Month}");
+Console.WriteLine($"Dia: {dataAtual.Day}");
+Console.WriteLine($"Hora: {dataAtual.Hour}");
+Console.WriteLine($"Minuto: {dataAtual.Minute}");
+Console.WriteLine($"Segundo: {dataAtual.Second}");
+Console.WriteLine($"Milissegundo: {dataAtual.Millisecond}");
+
+Console.WriteLine($"Adicionar dias: {dataAtual.AddDays(5)}");
+Console.WriteLine($"Adicionar meses: {dataAtual.AddMonths(2)}");
+Console.WriteLine($"Adicionar anos: {dataAtual.AddYears(1)}");
+Console.WriteLine($"Adicionar horas: {dataAtual.AddHours(10)}");
+
+Console.WriteLine($"Dia da semana: {dataAtual.DayOfWeek}");
+Console.WriteLine($"Dia do ano: {dataAtual.DayOfYear}");
+
+Console.WriteLine($"Data em formato longo: {dataAtual.ToLongDateString()}");
+Console.WriteLine($"Data em formato curto: {dataAtual.ToShortDateString()}");
+Console.WriteLine($"Hora em formato longo: {dataAtual.ToLongTimeString()}");
+Console.WriteLine($"Hora em formato curto: {dataAtual.ToShortTimeString()}");
+
+Console.WriteLine($"Data e hora formatadas: {dataAtual:yyyy-MM-dd HH:mm:ss}");
+
+Console.ReadKey();
---
+++ CSFundamentos1/TiposDeDados4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Definindo variáveis do tipo string, object e dynamic");
+Console.WriteLine();
+string nome = "Maria";
+String sobrenome = "Silva";
+
+Console.WriteLine($"O nome é: {nome} {sobrenome}");
+Console.WriteLine();
+
+object idade = 20;
+object valor1 = 8.55m;
+object nome1 = "João";
+dynamic ativa = true;
+dynamic letra = 'A';
+Console.WriteLine($"A idade é: {idade}");
+Console.WriteLine($"O valor1 é: {valor1}");
+Console.WriteLine($"O nome1 é: {nome1}");
+Console.WriteLine($"A ativa é: {ativa}");
+Console.WriteLine($"A letra é: {letra}");
+Console.WriteLine();
+
+Console.ReadLine();
